c2c5e63d0c50d09390e1a9bbe0dbc59161de39a7
ashr-aful007 <ashraful99980@gmail.com>
2023-07-16 20:46:22 +0600

    conditional rendering

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -3,9 +3,11 @@ import { BiListPlus } from "react-icons/bi";
 import {useDispatch} from "react-redux"
 import { addTOCart, removeFromCart } from "../redux/actionCreators/productAction";
 import {RiDeleteBin7Fill} from "react-icons/ri"
+import { useLocation } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
+  const {pathname} = useLocation()
 
   return (
     <div
@@ -25,24 +27,25 @@ const ProductCard = ({ product }) => {
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
-        <button
+      { !pathname.includes("cart") && ( <button
           className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
            onClick={() => dispatch(addTOCart(product))}
         >
           Add
-        </button>
-        <button
-          className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
+        </button>)}
+      {  pathname.includes("cart") && (    <button
+          className='bg-red-500 flex justify-center items-center rounded-full py-1 px-2 flex-1 text-white text-bold'
            onClick={() => dispatch(removeFromCart(product))}
         >
+          remove
           <RiDeleteBin7Fill/>
-        </button>
-        <button
+        </button>)}
+      { !pathname.includes("cart") && (  <button
           title='Add to wishlist'
           className='bg-indigo-500  py-1 px-2 rounded-full'
         >
           <BiListPlus className='text-white' />
-        </button>
+        </button>)}
       </div>
     </div>
   );
