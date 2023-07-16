85d3cbd08dde38b4e53165cccb60efb07dfa003a
ashr-aful007 <ashraful99980@gmail.com>
2023-07-16 19:16:34 +0600

    main

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
-import { useProducts } from "../context/ProductProvider";
-import { actionTypes } from "../state/ProductState/actionTypes";
+import {useDispatch} from "react-redux"
+import { addTOCart, removeFromCart } from "../redux/actionCreators/productAction";
+import {RiDeleteBin7Fill} from "react-icons/ri"
 
 const ProductCard = ({ product }) => {
-  const { dispatch } = useProducts();
+  const dispatch = useDispatch();
 
   return (
     <div
-      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
+      className='shadow-lg rounded-3xl border  p-4 flex flex-col text-indigo-900'
       key={product._id}
     >
       <div className='h-52 w-52 mx-auto'>
@@ -26,11 +27,15 @@ const ProductCard = ({ product }) => {
       <div className='flex gap-2 mt-5'>
         <button
           className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
-          onClick={() =>
-            dispatch({ type: actionTypes.ADD_TO_CART, payload: product })
-          }
+           onClick={() => dispatch(addTOCart(product))}
         >
-          Add to cart
+          Add
+        </button>
+        <button
+          className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
+           onClick={() => dispatch(removeFromCart(product))}
+        >
+          <RiDeleteBin7Fill/>
         </button>
         <button
           title='Add to wishlist'
